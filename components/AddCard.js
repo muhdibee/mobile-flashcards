7d0007d35da9873776d1e276f311d4c534aae910
Muhammad <muhdibee@gmail.com>
2021-11-22 16:57:33 +0100

    Created AddCard Component

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { View, Text, Stylesheet, Button } from 'react-native';
+import { View, Text, Button, Stylesheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import { appStyles } from '../appStylesheet/appStyles';
 import {white, gray, black, blue, lightgray} from '../utils/colors'
 
-class SelectedDeck extends React.Component {
 
+
+class AddDeck extends React.Component {
+
+    state = {
+        question:'',
+        answer: ''
+    }
+
+    handleOnChangeText(e) {
+        this.setState({text:e})
+        console.log('Text Value:', this.state.text)
+    }
     render() {
         return(
-        <View style={[appStyles.container, {justifyContent: 'space-around', flex: 1}]}>
-            <View style={[appStyles.container, {justifyContent: 'center', flex: 1}]}>
-                <Text style={{margin: 10}}>{'Deck 1'}</Text>
-                <Text style={{margin: 10}}>{'2'} cards</Text>
+            <View style={[appStyles.container, {justifyContent: "space-evenly", flex: 1}]}>
+                <View style={[appStyles.container, {justifyContent: "center", flex: 1}]}>
+                    <TextInput onChangeText={(e)=> this.handleOnChangeText(e)} value={this.state.question} style={{margin: 10}} placeholder="Question"/>
+                    <TextInput onChangeText={(e)=> this.handleOnChangeText(e)} value={this.state.answer} style={{margin: 10}} placeholder="Answer"/>
+                </View>
+                <View style={[appStyles.container, {justifyContent: "center", flex: 1}]}>
+                    <Button style={{ margin: 10, padding: 10 }} title="Create Deck" />
+                </View>
             </View>
-            <View style={[appStyles.container, {justifyContent: 'center', flex: 1}]}>
-                <Button style={{ margin: 10, padding: 10}} title='Add Card' />
-                <Button style={{ margin: 10, padding: 10}} title='Start Quiz' />
-                <Text style={{ padding: 10, margin: 10}} >Delete Deck</Text>
-            </View>
-        </View>
         )
     }
 
 }
 
-export default SelectedDeck;
+export default AddDeck;
--- components/AddDeck.js
+++ components/AddDeck.js
@@ -11,6 +11,7 @@ class AddDeck extends React.Component {
     state = {
         text:''
     }
+
     handleOnChangeText(e) {
         this.setState({text:e})
         console.log('Text Value:', this.state.text)
@@ -20,7 +21,7 @@ class AddDeck extends React.Component {
             <View style={[appStyles.container, {justifyContent: "space-evenly", flex: 1}]}>
                 <View style={[appStyles.container, {justifyContent: "center", flex: 1}]}>
                     <Text style={{margin: 10}}>What is the title of your new deck?</Text>
-                    <TextInput onChangeText={(e) => this.handleOnChangeText(e)} style={{margin: 10}} />
+                    <TextInput onChangeText={(e)=> this.handleOnChangeText(e)} value={this.state.text} style={{margin: 10}} placeholder="Deck Title"/>
                 </View>
                 <View style={[appStyles.container, {justifyContent: "center", flex: 1}]}>
                     <Button style={{ margin: 10, padding: 10 }} title="Create Deck" />
